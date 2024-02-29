89c755b20068c74844d692a2bb2b116d7ccae3a5
Andrew Mokhtary <akouroshm@gmail.com>
2024-02-29 14:23:04 -0500

    Added Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    [SerializeField] GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Debug.Log("Worked");
+    }
+    public void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Debug.Log("Unpaused");
+    }
+}
