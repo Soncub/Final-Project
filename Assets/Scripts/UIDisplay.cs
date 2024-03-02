93a99e27ddcb07a668bcf2f0f2de00a16f8b5972
JW419 <148804427+JW419@users.noreply.github.com>
2024-03-01 23:31:26 -0500

    UI branch
    Committing this to the UI branch

---
+++ Assets/Scripts/UIDisplay.cs
@@ -0,0 +1,44 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDisplay : MonoBehaviour
+{
+    public static UIDisplay instance;
+    //accessing the PlayerController script
+    public GameObject Player;
+    public PlayerController playercontroller;
+
+    public Text scoreText;
+    public Text healthText;
+
+    int score = 0;
+    int health = 0;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        playercontroller = Player.GetComponent<PlayerController>();
+        health = playercontroller.maxHealth;
+        scoreText.text = "Score " + score.ToString();
+        healthText.text = "Health " + health.ToString();
+    }
+
+    public void AddPoint()
+    {
+        score += 1;
+        scoreText.text = "SCORE " + score.ToString();
+    }
+
+    public void RemoveHealth()
+    {
+        health -= 1;
+        healthText.text = "Health " + health.ToString();
+    }
+}
