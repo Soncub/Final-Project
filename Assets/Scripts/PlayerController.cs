f5121d13faf1608f7df89b34ce597ee569109c83
Zorphyo <zorphyo@outlook.com>
2024-04-04 19:28:50 -0400

    Lab Assignment 12 additions (XML and JSON)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Text;
+using Packages.Rider.Editor.UnitTesting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -23,6 +30,88 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] GameObject gameOver;
     public static bool GameOvered = false;
+
+    //the following variables are only for Lab Assignment 12.
+    private string _dataPath;
+    private string _textFile;
+    private string _xmlWeapons;
+    private string _jsonWeapons;
+
+    [Serializable]
+    public struct TestWeapon
+    {
+        public string name;
+        public int damage;
+
+        public TestWeapon(string name, int damage)
+        {
+            this.name = name;
+            this.damage = damage;
+        }
+    }
+
+    private List<TestWeapon> weapons = new List<TestWeapon>
+    {
+        new TestWeapon("Fireball", 10),
+        new TestWeapon("Lightning Blast", 15),
+        new TestWeapon("Basic Sword", 5),
+    };
+
+    //for Lab Assignment 12. Can probably remove or repurpose later
+    void Awake()
+    {
+        _dataPath = Application.persistentDataPath + "/Player_Data/";
+        Debug.Log(_dataPath);
+        _textFile = _dataPath + "Save_Data.txt";
+
+        if (Directory.Exists(_dataPath))
+        {
+            Debug.Log("Directory already exists.");
+        }
+
+        else
+        {
+            Directory.CreateDirectory(_dataPath);
+            Debug.Log("New Directory Created!");
+        }
+
+        if (File.Exists(_textFile))
+        {
+            Debug.Log("File already exists.");
+        }
+
+        else
+        {
+            File.WriteAllText(_textFile, "<SAVE DATA>\n");
+            Debug.Log("New File Created!");
+        }
+
+        _xmlWeapons = _dataPath + "Weapons.xml";
+
+        var xmlSerializer = new XmlSerializer(typeof(List<TestWeapon>));
+
+        using (FileStream stream = File.Create(_xmlWeapons))
+        {
+            xmlSerializer.Serialize(stream, weapons);
+        }
+
+        Debug.Log("Created XML File.");
+
+        _jsonWeapons = _dataPath + "Weapons.json";
+
+        string jsonString1 = JsonUtility.ToJson(weapons[0], true);
+        string jsonString2 = JsonUtility.ToJson(weapons[1], true);
+        string jsonString3 = JsonUtility.ToJson(weapons[2], true);
+
+        using (StreamWriter stream = File.CreateText(_jsonWeapons))
+        {
+            stream.WriteLine(jsonString1);
+            stream.WriteLine(jsonString2);
+            stream.WriteLine(jsonString3);
+        }
+
+        Debug.Log("Created JSON File.");
+    }
 
     // Start is called before the first frame update
     void Start()
